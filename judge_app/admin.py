902fc84a297258cd188905c933adb3700a70c026
Denver Smith <denverpsmith@gmail.com>
2016-02-03 20:09:49 -0700

    updating models

--- judge_app/admin.py
+++ judge_app/admin.py
@@ -11,20 +11,40 @@ from .models import Student, Project
 # Register your models here.
 
 class StudentResource(resources.ModelResource):
-    project = fields.Field(column_name='project',
+    project = fields.Field(column_name='Project Title',
         attribute='project',
         widget=ForeignKeyWidget(Project, 'title'))
+    first_name = fields.Field(column_name="Fname",
+        attribute='first_name') 
+    last_name = fields.Field(column_name="Lname",
+        attribute='last_name')
+    response_id = fields.Field(column_name="ResponseID",
+        attribute='response_id')
+    school = fields.Field(column_name="School",
+        attribute='school')
+    phone = fields.Field(column_name="Contact Phone",
+        attribute='phone')
+    gender = fields.Field(column_name="Gender",
+        attribute='gender')
+    grade_level = fields.Field(column_name="Grade Level",
+        attribute='grade_level')
     
     class Meta:
         model = Student
         skip_unchanged = True
-        fields = ('id', 'name', 'project')
+        import_id_fields = ('response_id',)
+        #fields = ('response_id', 'Fname', 'project')
 
 class ProjectResource(resources.ModelResource):
+    project_id = fields.Field(column_name="Project ID",
+        attribute='project_id')
+    title = fields.Field(column_name="Project Title",
+        attribute='title')
     class Meta:
         model = Project
         skip_unchanged = True
-        fields = ('id','title')
+        import_id_fields = ('project_id',)
+        fields = ('project_id','title')
 
 class StudentAdmin(ImportExportModelAdmin):
     resource_class = StudentResource
--- judge_app/models.py
+++ judge_app/models.py
@@ -4,13 +4,19 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=100)
+    response_id = models.CharField(primary_key=True, max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    school = models.CharField(max_length=100)
+    phone = models.CharField(max_length=50)
+    grade_level = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
     project = models.ForeignKey('Project', on_delete=models.CASCADE)
     # Student has 1 project. A project can have many students.
 
     def __unicode__(self):
-        return self.name
-"""
+        return self.first_name + " " + self.last_name
+
 class Award(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
@@ -20,12 +26,13 @@ class Award(models.Model):
     judge_count = models.IntegerField(default=1)
     total_evals_per_student = models.IntegerField(default=1)
     remaining_judge_evals = models.IntegerField(default=1)
-"""
+
 class Project(models.Model):
     # A project enters 1 award. An award can have many entered projects.
     #registered_award = models.ForeignKey('Award', on_delete=models.CASCADE)
+    project_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=250)
-    score = models.IntegerField(default=1)
+    score = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.title
