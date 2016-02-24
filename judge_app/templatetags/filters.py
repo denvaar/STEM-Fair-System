ff474c78feb63ee64a50d9071660e9356a3cb9fb
Denver Smith <denverpsmith@gmail.com>
2016-02-23 21:53:10 -0700

    trying template tag to order winners

--- judge_app/templatetags/filters.py
+++ judge_app/templatetags/filters.py
@@ -1,12 +1,16 @@
 from django import template
 
 register = template.Library()
-@register.filter(name='css')
-@register.filter(name='placeholder')
 
+@register.filter(name='css')
 def css(field, css):
     return field.as_widget(attrs={"class":css})
 
+@register.filter(name='placeholder')
 def placeholder(value, token):
     value.field.widget.attrs["placeholder"] = token
     return value
+
+@register.filter(name='sort_by')
+def sort_by(queryset, order):
+    return queryset.order_by(order)
